4e61ab10f6aa1259d18210b2f95b5b7ab44eeafa
grozek <roznawsk@grinnell.edu>
2024-11-12 14:11:37 -0600

    first commit

---
+++ project3.hpp
@@ -0,0 +1,31 @@
+/*
+ * CSC-301
+ * project3.hpp
+ * Fall 2022
+ *
+ * Partner 1:
+ * Partner 2:
+ * Date:
+ */
+
+#ifndef PROJECT3_HPP_
+#define PROJECT3_HPP_
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+/*
+ * createAdjacencyMatrix
+ */
+std::vector<double> createAdjacencyMatrix(std::vector<double> &rates,
+                                          std::vector<std::string> &currencies);
+
+/*
+ * detectArbitrage
+ */
+std::vector<int> detectArbitrage(std::vector<double> &adjMatrix,
+                                 std::vector<std::string> &currencies,
+                                 double tol=1e-15);
+
+#endif  // PROJECT3_HPP_
